a219ee76132ada63dcb06db76a08a4a460040a42
Lucass <lucasaroucaalvim041006@gmail.com>
2025-04-02 19:44:03 -0300

    Dia 3

---
+++ projeto-3/script.js
@@ -0,0 +1,35 @@
+let rodada = 1;
+let venceu = false
+
+while (rodada<=3){
+    console.log ("Rodada:" + rodada);
+
+    escolha_jogador = parseFloat(prompt(`Nível ${rodada}, escolha um vidro (1,2 ou 3)?:  `));
+    piso_quebrado = Math.floor(Math.random() * 3) + 1;
+
+    if(escolha_jogador <= 0 || escolha_jogador > 3){
+        alert("Digite um valor válido!")
+        continue
+    }
+
+
+    if (escolha_jogador == piso_quebrado){
+        alert("Piso quebrou!! Você perdeu!!")
+        rodada = 1000;
+        break
+    } else {
+        alert("Você passou!")
+    }
+
+    rodada = rodada + 1;
+3
+    if(rodada>=4){
+        venceu = true
+    }
+}
+
+// Depois da rodada 3, vem apra cá
+
+if (venceu == true){
+    alert("PARABÉNS VOCE GANHOU!!")
+}
